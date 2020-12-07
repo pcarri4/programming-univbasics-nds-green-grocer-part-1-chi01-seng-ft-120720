4734a4efb5d1e63d8fdd11153157c05d63c693fe
Paulina Carrillo <paulinacarri4@gmail.com>
2020-12-07 00:06:41 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -2,7 +2,17 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
-
+	[
+		{"AVOCADO" => {:price => 3.00, :clearance => true}},
+		{"KALE" => {:price => 3.00, :clearance => false}},
+		{"BLACK_BEANS" => {:price => 2.50, :clearance => false}},
+		{"ALMONDS" => {:price => 9.00, :clearance => false}},
+		{"TEMPEH" => {:price => 3.00, :clearance => true}},
+		{"CHEESE" => {:price => 6.50, :clearance => false}},
+		{"BEER" => {:price => 13.00, :clearance => false}},
+		{"PEANUTBUTTER" => {:price => 3.00, :clearance => true}},
+		{"BEETS" => {:price => 2.50, :clearance => false}}
+	]
 end
 
 def consolidate_cart(cart)
